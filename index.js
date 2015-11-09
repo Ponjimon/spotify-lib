520b89a7fa57c1d3a9e787b5ac141dc103510a45
lookapanda <admin@thurnax.com>
2015-11-09 18:38:59 +0100

    1.5.0

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-﻿var request = require("request");
+﻿﻿ var request = require("request");
 var util = require("util");
 var events = require("events");
 
@@ -8,8 +8,9 @@ var SpotifyClient = function () {
     var self = this;
     self.base = "https://kdghnzhmwr.spotilocal.com:4370";
     self.headers = { Origin: "https://open.spotify.com" };
-
-    request({url : "https://open.spotify.com/token"}, function (error, response, body) {
+    self.isPlaying = false;
+    request({ url : "https://open.spotify.com/token" }, function (error, response, body) {
+        if (error) throw error;
         self.oauth = JSON.parse(body).t;
         var options = {
             url: self.base + "/simplecsrf/token.json",
@@ -17,7 +18,16 @@ var SpotifyClient = function () {
             headers: self.headers
         };
         request(options, function (error, response, body) {
-            self.token = JSON.parse(body).token;
+            if (error) {
+                console.log(error);
+                var error2 = {};
+                error2.type = "4110";
+                self.emit("error", error2);
+                throw error;
+            }
+            var obj = JSON.parse(body);
+            if ('error' in obj && obj.error.type == 4110) { self.emit("error", { type: "4110" }); return; }
+            self.token = obj.token;
             self.init();
         });
     });
@@ -28,14 +38,23 @@ util.inherits(SpotifyClient, events.EventEmitter);
 SpotifyClient.prototype.init = function () {
     this.emit("ready");
 }
-SpotifyClient.prototype.status = function () {
+
+SpotifyClient.prototype.status = function (ret) {
     var self = this;
+    ret = ret || '';
+    if (ret === true) ret = '&returnon=login,logout,play,pause,error,ap&returnafter=60';
     request({
-        url: self.base + "/remote/status.json?csrf=" + self.token + "&oauth=" + self.oauth,
+        url: self.base + "/remote/status.json?csrf=" + self.token + "&oauth=" + self.oauth + ret,
         rejectUnauthorized: false,
         headers: self.headers
     }, function (error, response, body) {
-        self.emit("status", body);
+        if (error) throw error;
+        body = JSON.parse(body);
+        if (body.error) {
+            self.emit("error", body.error);
+        } else {
+            self.emit("status", body.playing, body.playing_position, body.track, body.volume, body);
+        }
     });
 }
 
@@ -46,22 +65,80 @@ SpotifyClient.prototype.play = function (spotifyUri) {
         rejectUnauthorized: false,
         headers: self.headers
     }, function (error, response, body) {
+        if (error) throw error;
         body = JSON.parse(body);
-        self.emit("play", body.track.track_resource.name, body);
+        if (body.error) {
+            self.emit("error", body.error);
+        } else {
+            self.emit("play", body.track.track_resource.name, body);
+        }
     });
 }
 
-SpotifyClient.prototype.pause = function () {
+SpotifyClient.prototype.pause = function (pause) {
     var self = this;
     request({
-        url: self.base + "/remote/pause.json?csrf=" + self.token + "&oauth=" + self.oauth + "&pause=true",
+        url: self.base + "/remote/pause.json?csrf=" + self.token + "&oauth=" + self.oauth + "&pause=" + pause,
         rejectUnauthorized: false,
         headers: self.headers
     }, function (error, response, body) {
+        if (error) throw error;
         body = JSON.parse(body);
-        self.emit("pause", body.track.track_resource.name, body);
+        if (body.error) {
+            self.emit("error", body.error);
+        } else {
+            self.emit("pause", body.track.track_resource.name, body);
+        }
     });
 }
+
+SpotifyClient.prototype.browse = function (spotifyUri, cb) {
+    var self = this;
+    request({
+        url: self.base + "/remote/browse.json?csrf=" + self.token + "&oauth=" + self.oauth + "&context=" + spotifyUri + "&uri=" + spotifyUri,
+        rejectUnauthorized: false,
+        headers: self.headers
+    }, function (error, response, body) {
+        if (error) throw error;
+        body = JSON.parse(body);
+        if (body.error) {
+            self.emit("error", body.error);
+            cb(body);
+        } else {
+            self.emit("browse", body);
+            cb(body);
+        }
+    });
+}
+
+SpotifyClient.prototype.auth = function (username, password, cb) {
+    var self = this;
+    var headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.101 Safari/537.11'
+    };
+    request({
+        url: 'https://play.spotify.com/redirect/facebook/notification.php?album=http://open.spotify.com/album/2mCuMNdJkoyiXFhsQCLLqw&song=http://open.spotify.com/track/6JEK0CvvjDjjMUBFoXShNZ',
+        headers: headers
+    }, function (error, response, body) {
+        if (error) throw error;
+        var token = body.match(/csrftoken":"([a-zA-Z0-9]{32})/)[1];
+        request.post({
+            url: 'https://play.spotify.com/xhr/json/auth.php', headers: headers,
+            form: {
+                type: 'sp',
+                username: username,
+                password: password,
+                secret: token
+            }
+        }, function (error, response, body) {
+            if (error) throw error;
+            body = JSON.parse(body);
+            cb(body);
+        });
+    });
+}
+
+
 
 
 module.exports = SpotifyClient;
